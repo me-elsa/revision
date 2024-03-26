c63a191a63c7f77529e7baa7993e061736fb1d5d
me-elsa <elsaa.butt@gmail.com>
2024-03-27 00:46:24 +0500

    hell

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-Helloooow
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+//1
+const app = express()
+app.get('/',(req,res)=> {
+    res.send({
+        message: "welcome g"
+    })
+})
+//2
+//port 
+const PORT  = 8080
+//run listen
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}...`.bgMagenta.white);})
+
+
